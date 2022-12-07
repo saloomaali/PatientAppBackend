742de096785fd4c5b958528062b87fd156c248ce
Salooma <saloomaali2000@gmail.com>
2022-12-07 07:50:47 +0530

    add route

--- src/main/java/com/Nest/PatientApp/controller/PatientController.java
+++ src/main/java/com/Nest/PatientApp/controller/PatientController.java
@@ -1,9 +1,18 @@
 package com.Nest.PatientApp.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PatientController {
 
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public String addPatient(){
+
+        return "patient added successfully";
+    }
 
 }
