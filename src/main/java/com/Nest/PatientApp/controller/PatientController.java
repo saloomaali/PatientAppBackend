15c777323afcb495525fc4b5b389e099d881172c
Salooma <saloomaali2000@gmail.com>
2022-12-08 15:00:12 +0530

    completed

--- src/main/java/com/Nest/PatientApp/controller/PatientController.java
+++ src/main/java/com/Nest/PatientApp/controller/PatientController.java
@@ -5,6 +5,7 @@ import com.Nest.PatientApp.model.Patients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -16,14 +17,15 @@ public class PatientController {
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
-    public String addPatient(@RequestBody Patients p){
+    public HashMap<String, String> addPatient(@RequestBody Patients p){
         System.out.println(p.getPtn_Id());
         System.out.println(p.getName());
         System.out.println(p.getAge());
         System.out.println(p.getMobNo());
         dao.save(p);
-
-        return "patient added successfully";
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "success");
+        return map;
     }
 
     @CrossOrigin(origins = "*")
@@ -32,5 +34,26 @@ public class PatientController {
 
         return (List<Patients>) dao.findAll();
     }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search", consumes = "application/json", produces = "application/json")
+    public List<Patients> searchPatient(@RequestBody Patients p){
+
+        System.out.println(p.getPtn_Id());
+        return (List<Patients>) dao.searchPatient(p.getPtn_Id());
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/delete", consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> deletePatient(@RequestBody Patients p){
+
+        System.out.println(p.getId());
+        dao.deletePatient(p.getId());
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "success");
+        return map;
+    }
+
+
 
 }
